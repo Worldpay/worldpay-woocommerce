600488f9dc0ee797aa555fa8abb559326095ae8c
ao <andrew@hsmoore.com>
2015-05-27 14:59:58 +0100

    Change to lowercase token.php

---
+++ Persistence/token.php
@@ -0,0 +1,18 @@
+<?php
+
+class Worldpay_Token
+{
+	public static function get_by_user($user)
+	{
+		if($user == null){
+			return null;
+		}
+
+		$tokens = get_user_meta($user->ID, 'worldpay_token');
+		if(!is_array($tokens) || count($tokens) == 0){
+			return null;
+		}
+
+		return $tokens[0];
+	}
+} 
